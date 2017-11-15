42bc558643a267f4ca7fe941e6cf800d11ffc14c
Joe Anderson <joejbajoe@gmail.com>
2017-11-15 10:48:07 +0000

    Take count and sides from argv

--- roller/src/main.c
+++ roller/src/main.c
@@ -2,13 +2,24 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
+  if (argc < 3) {
     printf("missing argument(s)\n");
     return 1;
   }
 
-  int n = atoi(argv[1]);
-  printf("%d\n", n);
+  int dice_count = atoi(argv[1]);
+  if (dice_count == 0) {
+    printf("cannot roll zero dice\n");
+    return 1;
+  }
+
+  int dice_sides = atoi(argv[2]);
+  if (dice_sides == 0) {
+    printf("I rolled this zero-sided die for you. You got a 1.\n");
+    return 1;
+  }
+
+  printf("I am rolling %dd%d", dice_count, dice_sides);
 
   return 0;
 }
