ce6f0e500c9ffe76364dc5fea4a2405db3cbd798
Joe Anderson <joejbajoe@gmail.com>
2017-11-15 13:36:37 +0000

    Wrap binary in ruby module

---
+++ scheduler/src/roller.rb
@@ -0,0 +1,14 @@
+module Roller
+  def self.roll(count, type)
+    safe_count = count.to_i.to_s # escape any nasty "; sudo halt"s
+    safe_type  = type.to_i.to_s
+    return `#{bin_path} #{safe_count} #{safe_type}`
+  end
+
+  private
+
+  def self.bin_path
+    File.join(__dir__, "..", "bin", "rollpaths")
+  end
+end
+
